cf68aeed0eee0ac31a628710168dceff14968c79
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-04 16:50:27 +0700

    Add Example009_square_true

---
+++ Example009_square_true/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write ("Введите число, ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите число, ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = numberA / numberB;
+if (result == numberA)
+{
+    Console.WriteLine ("Не является корнем ");
+}
+else
+{ 
+    Console.WriteLine ("Ура, это корень!");
+}
