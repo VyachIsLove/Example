1ad3f681e8816a0c02fd1e7e8948af6b9021e457
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-04 18:53:53 +0700

    Add example 010 more less

---
+++ Example010_more_less/Program.cs
@@ -0,0 +1,14 @@
+﻿int numberA = 10;
+int numberB = 7;
+
+int max = numberA;
+int min = numberB;
+
+
+if (numberA > max) max = numberA; 
+else min = numberA;
+if (numberB > max) max = numberB; 
+else min = numberB;
+
+Console.WriteLine ("Наибольшее число "+ max );
+Console.WriteLine ("Наименьшее число "+ min);
