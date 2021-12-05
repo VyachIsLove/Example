c479afee9cc0c617b67d5b48f487d4b499efe0c5
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-05 13:58:09 +0700

    Add Example013 Method Array

---
+++ Example013_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 1, 12, 32, 41, 18, 15, 62, 17, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+        {
+            Console.WriteLine(index);
+            break;
+        }
+        
+    // index = index + 1;
+    index++;
+}
