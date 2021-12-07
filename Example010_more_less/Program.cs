9d4f3998975dcd84c22d6a3791f28f29c8fb7366
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-07 14:31:55 +0700

    Fix Example010 more less

--- Example010_more_less/Program.cs
+++ Example010_more_less/Program.cs
@@ -1,14 +1,20 @@
-﻿int numberA = 10;
-int numberB = 7;
+﻿int numberA = 3;
+int numberB = 35;
 
 int max = numberA;
 int min = numberB;
 
+if(numberA != numberB)
+{
+    if (numberA > max) max = numberA; 
+    else min = numberA;
+    if (numberB > max) max = numberB; 
+    else min = numberB;
 
-if (numberA > max) max = numberA; 
-else min = numberA;
-if (numberB > max) max = numberB; 
-else min = numberB;
-
-Console.WriteLine ("Наибольшее число "+ max );
-Console.WriteLine ("Наименьшее число "+ min);
+    Console.WriteLine ("Наибольшее число "+ max );
+    Console.WriteLine ("Наименьшее число "+ min);
+}
+if(numberA == numberB)
+{
+    Console.WriteLine("Числа равны");
+}
