cd55d9321682e84f45542ae434ffc352d6bbab78
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-08 17:17:49 +0700

    Add Example015 days of the week

---
+++ Example015_Days_Week/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter the number ");
+int days = Convert.ToInt32(Console.ReadLine());
+
+if (days == 1) Console.WriteLine("Monday");
+
+if (days == 2) Console.WriteLine("Tuesday");
+
+if (days == 3) Console.WriteLine("Wednesday");
+
+if (days == 4) Console.WriteLine("Thursday");
+
+if (days == 5) Console.WriteLine("Friday");
+
+if (days == 6) Console.WriteLine("Saturday");
+    
+if (days == 7) Console.WriteLine("Sunday");
