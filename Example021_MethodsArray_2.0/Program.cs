3e8891107dcc4d167292f554f63f27b84878202d
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-11 18:49:12 +0700

    Add Example021 Method Array 2.0

---
+++ Example021_MethodsArray_2.0/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] arr = {7, 5, 4, 8, 2, 7, 3, 6, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
