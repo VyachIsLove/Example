04113a76a54ca1b92b6290db9c259a312af1def2
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-13 18:44:40 +0700

    Fix Task007

--- Task/Task007_num_-N_N/Program.cs
+++ Task/Task007_num_-N_N/Program.cs
@@ -1,11 +1,10 @@
 ﻿// Показать числа от -N до N
 
-int[] num = {-9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-int count = num.Length;
-int i = 0;
-while(i < count)
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = -N;
+while(a < N+1)
 {
-    Console.WriteLine(num[i]);
-    i++;
+    Console.WriteLine(a);
+    a++;
 }
