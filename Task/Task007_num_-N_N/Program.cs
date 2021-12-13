5cebb9eceabe0af59d7534ecfd1ba00125c25181
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-13 17:20:32 +0700

    Fix Task007

--- Task/Task007_num_-N_N/Program.cs
+++ Task/Task007_num_-N_N/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать числа от -N до N
+
+int[] num = {-9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+int count = num.Length;
+int i = 0;
+while(i < count)
+{
+    Console.WriteLine(num[i]);
+    i++;
+}
