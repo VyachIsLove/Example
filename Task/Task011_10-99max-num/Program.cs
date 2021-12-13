c2edbc205a5abcda021ab282b6ec3a71a8928af3
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-13 20:08:15 +0700

    Add Task011

---
+++ Task/Task011_10-99max-num/Program.cs
@@ -0,0 +1,10 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int a = new Random().Next(10, 99);
+
+Console.WriteLine(a);
+Console.WriteLine();
+
+
+
+Console.WriteLine(max);
