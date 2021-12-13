29677a19693686884440182fa2f91861bad829a4
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-13 18:21:24 +0700

    Add Task008 num 1 N

---
+++ Task/Task008_numN_even/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать четные числа от 1 до N
+
+int a = 1;
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+while (a < N+1)
+{
+    if (a % 2 == 0) Console.WriteLine(a);
+    a++;
+}
