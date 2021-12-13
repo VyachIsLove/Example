1f2750af4829cfb39b5e9612e625626e3268b8af
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-13 19:16:04 +0700

    Add Task009 and Task010

---
+++ Task/Task009_Three-digit_num/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.Write("Введите число: ");
+string text = Console.ReadLine();
+int n = text[text.Length - 1] - '0';
+
+Console.WriteLine(n);
---
+++ Task/Task010_2num_three-digit_num/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите число: ");
+string text = Console.ReadLine();
+int n = text[text.Length - 2] - '0';
+
+Console.WriteLine(n);
