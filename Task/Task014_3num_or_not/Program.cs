430ded10e2c9556bf9d8c1cf0880c5398e643798
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-17 21:39:19 +0700

    Add Task014 3num or not

---
+++ Task/Task014_3num_or_not/Program.cs
@@ -0,0 +1,32 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Write("Введите число ");
+string num = Console.ReadLine();
+
+if(num.Length == 3)
+{
+    int n = num[num.Length - 1] - '0';
+    Console.WriteLine(n);
+}
+else if (num.Length > 3)
+{
+    int n = num[num.Length - 2] - '0';
+    Console.WriteLine(n);
+}
+else
+{
+    Console.WriteLine("Третья цифра в числе отсутствует.");
+}
+
+// int n = num[num.Length -1] - '0';
+
+// if(num.Length == 3) 
+// Console.WriteLine(n);
+// else (num.Length < 3);
+// {
+//     Console.WriteLine("Третья цифра в числе отсутствует.");
+// }
+
+// if(num.Length > 3) 
+// int n = num[num.Length -2] - '0';
+// Console.WriteLine(n);
