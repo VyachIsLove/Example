07105c5d56cb7dbf050a3be8249adf8e321023d9
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-17 16:58:15 +0700

    Add Task013 Multiple with remainder

---
+++ Task/Task013_Multiple_with_remain/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.WriteLine("Введите число ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите делитель ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int remain = numA % numB;
+
+if(remain != 0) Console.WriteLine("Остаток равен "+ remain);
+if(remain == 0) Console.WriteLine("Число кратно заданному.");
