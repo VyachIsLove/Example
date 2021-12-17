ef860d4d1320e01aa9ab9f8f34be7f3e34bea6c9
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-17 15:29:41 +0700

    Add Task012 Delete second number

---
+++ Task/Task012_Del_second_num_three-digit_num/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.Write("Введите число: ");
+string text = Console.ReadLine();
+int n = text[text.Length - 3] - '0';
+int a = text[text.Length - 1] - '0';
+
+Console.Write(n); Console.Write(a);
