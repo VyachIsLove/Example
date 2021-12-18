066dace48f23405b6c678802cc1ee544b45bd022
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-18 15:50:03 +0700

    Add Task017 One root of the second

---
+++ Task/Task017_One_root_second/Program.cs
@@ -0,0 +1,23 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine("Введите первое число");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Введите второе число");
+int numB = Convert.ToInt32(Console.ReadLine());
+int result = numA / numB;
+int result_sec = numB / numA;
+
+if (result == numB)
+{
+    Console.WriteLine ("Второе число является корнем первого ");
+}
+else
+if(result_sec == numA)
+{
+    Console.WriteLine ("Первое число является корнем второго ");
+}
+else 
+{
+    Console.WriteLine("Ни одно из чисел не является корнем другого");
+}
