c4da4a05379a514b88db1d62c6d5b41204b25a68
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-18 19:10:01 +0700

    Add Task018 Truth statement

---
+++ Task/Task018_Truth_state/Program.cs
@@ -0,0 +1,9 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+bool x = true;
+bool y = false;
+
+if ( !(x | y) == !x & !y )
+Console.WriteLine("Утверждение верно");
+else
+Console.WriteLine("Утверждене ложно");
