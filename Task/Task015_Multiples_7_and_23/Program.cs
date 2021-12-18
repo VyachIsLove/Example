1307a98c0e0c4f398ad3247a7ad5f4f5272aeb78
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-18 12:33:25 +0700

    Add Task015 Multiples 7 and 23

---
+++ Task/Task015_Multiples_7_and_23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int resA = num % 7;
+int resB = num % 23;
+
+if( resA == 0 ) Console.WriteLine("Число кратно 7");
+else 
+Console.WriteLine("Число не кратно 7");
+
+if( resB == 0 ) Console.WriteLine("Число кратно 23");
+else 
+Console.WriteLine("Число не кратно 23");
