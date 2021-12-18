2f92d6ec9b7dae2ab54aeb2177c577eabfbbf6ca
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-18 13:23:19 +0700

    Add Task016 Weekend number

---
+++ Task/Task016_Weekend_number/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+int day = new Random().Next(1, 7);
+Console.WriteLine(day);
+Console.WriteLine();
+
+if ( day == 6 ) Console.WriteLine("Выходной!");
+else
+
+if ( day == 7 ) Console.WriteLine("Выходной!");
+else
+Console.WriteLine("Будний день");
