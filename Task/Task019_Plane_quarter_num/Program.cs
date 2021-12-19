ca680f37de8439fa1307f60bf0eab686baba5c8a
[VyachIsLove] <[slava_nepom96@mail.ru]>
2021-12-19 11:32:49 +0700

    fix task019 plane quarter

---
+++ Task/Task019_Plane_quarter_num/Program.cs
@@ -0,0 +1,30 @@
+﻿// Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+Console.WriteLine("Введите координату X ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if ( x != 0 && y != 0)
+{
+    if ( x > 0 && y > 0)
+    {
+        Console.WriteLine("I четверть плоскости");
+    }
+        if ( x < 0 && y > 0 )
+        {
+            Console.WriteLine("II четверть плоскости");
+        }
+            if ( x < 0 && y < 0 )
+            {
+                Console.WriteLine("III четверть плоскости");
+            }
+                if ( x > 0 && y < 0 )
+                {
+                    Console.WriteLine("IV четверть плоскости");
+                }
+}
+else 
+Console.WriteLine("Произведение координат не должно ровняться 0");
+
+
